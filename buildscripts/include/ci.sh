9e4d7043223f67a5f03704d9c09045ee46a0e56e
spvkgn <spvkgn@users.noreply.github.com>
2025-09-20 18:10:27 +0500

    test

--- buildscripts/include/ci.sh
+++ buildscripts/include/ci.sh
@@ -81,16 +81,16 @@ fi
 
 msg "Fetching python"
 mkdir -p $HOME/dist
-$WGET https://github.com/spvkgn/ndk-pkg-package-manually-build/releases/download/python3.10-release/python3-3.10.18-android-21-arm64-v8a.release.tar.xz -O - | tee \
-	>(tar -C ../app/src/main/assets/ytdl --strip-components=2 --transform='s/python3.10/python3/' --wildcards "*/bin/python3.10" -xJ) \
-	>(tar -C $HOME/dist --strip-components=3 --wildcards "*/lib/python3.10/" -xJ) >/dev/null
+$WGET https://github.com/spvkgn/ndk-pkg-package-manually-build/releases/download/python3.11-release/python3-3.11.13-android-21-arm64-v8a.release.tar.xz -O - | tee \
+	>(tar -C ../app/src/main/assets/ytdl --strip-components=2 --transform='s/python3.11/python3/' --wildcards "*/bin/python3.11" -xJ) \
+	>(tar -C $HOME/dist --strip-components=3 --wildcards "*/lib/python3.11/" -xJ) >/dev/null
 # $WGET https://github.com/spvkgn/ndk-pkg-package-manually-build/releases/download/python3.9-release/python3-3.9.22-android-21-arm64-v8a.release.tar.xz -O - | tee \
 # 	>(tar -C ../app/src/main/assets/ytdl --strip-components=2 --transform='s/python3.9/python3/' --wildcards "*/bin/python3.9" -xJ) \
 # 	>(tar -C $HOME/dist --strip-components=3 --wildcards "*/lib/python3.9/" -xJ) >/dev/null
 
 recompile_py () {
 	find . -name '*.pyc' -delete
-	python3.10 -OO -m compileall -b -j$(nproc) .
+	python3.11 -OO -m compileall -b -j$(nproc) .
 	# python3.9 -OO -m compileall -b -j$(nproc) .
 	# leave only the legacy locations (*.pyc next to *.py)
 	find . -name "__pycache__" -print0 | xargs -0 -- rm -rf
@@ -101,7 +101,7 @@ prune_stdlib () {
 		pydoc_data turtledemo # docs
 		test unittest/test # unittests
 		tkinter sqlite3 venv ensurepip # doesn't work anyway
-		lib2to3 idlelib distutils multiprocessing # not used by ytdl
+		lib2to3 idlelib distutils multiprocessing tomllib # not used by ytdl
 	)
 	rm -rf "${delete[@]}"
 	# ytdl tries to import this:
@@ -118,7 +118,7 @@ FILE
 	cd $HOME/dist
 	prune_stdlib
 	recompile_py
-	zip -9 $GITHUB_WORKSPACE/app/src/main/assets/ytdl/python310.zip -R '*.pyc'
+	zip -9 $GITHUB_WORKSPACE/app/src/main/assets/ytdl/python311.zip -R '*.pyc'
 	# zip -9 $GITHUB_WORKSPACE/app/src/main/assets/ytdl/python39.zip -R '*.pyc'
 )
 
